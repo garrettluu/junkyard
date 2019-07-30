d2f93a9559309d80903d4451ae944a45d253d868
Garrett Luu <garrettluu131@gmail.com>
2019-07-30 01:37:11 -0700

    Basic vim mode implementation

--- keymap.c
+++ keymap.c
@@ -1,30 +1,40 @@
 #include "keymap_common.h"
 
 const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    //Base keymap
     KEYMAP(ESC,  1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   BSPC, \
            TAB,  Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,    ENT, \
            LGUI, A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT, \
            LSFT, Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, UP,  RSFT, \
            LCTL, LGUI,FN3, LALT, FN1, FN2,     LALT, FN4,LEFT,DOWN,RIGHT),
 
+    //Layer 1
     KEYMAP( GRV,   F1,   F2,   F3,   F4,   F5,   F6,   F7,   F8,   F9,  F10,  DEL, \
             INS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, LBRC, RBRC, TRNS, BSLS, \
            CAPS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,  FN5,  FN6, TRNS, TRNS, \
            TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, SLSH, TRNS, \
            TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,       TRNS, RCTL, TRNS, TRNS, FN7),
 
+    //Layer 2
     KEYMAP(TRNS,  F11,  F12, VOLD, VOLU, TRNS, TRNS, TRNS, TRNS, MINS,  EQL, TRNS, \
            TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
            TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
            TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
            TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,       TRNS, TRNS, TRNS, TRNS, TRNS),
 
+    //Game mode, makes space bar hold-able
     KEYMAP(TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
            TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
            TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
            TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
            TRNS, TRNS, TRNS, TRNS, TRNS, SPC,        FN0,  TRNS,  TRNS, TRNS, TRNS),
 
+    //Vim mode
+    KEYMAP(TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
+           TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,  FN9, TRNS, TRNS, TRNS, TRNS, \
+           TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, LEFT, DOWN,   UP,RIGHT, TRNS, TRNS, \
+           TRNS, TRNS,  DEL, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
+           TRNS, TRNS, TRNS, TRNS, TRNS, TRNS,       TRNS, TRNS, TRNS, TRNS, TRNS),
 
     /* Blank keymap for function layers
      * KEYMAP(TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, TRNS, \
@@ -39,7 +49,8 @@ enum macro_id {
 	TERM,
 	LCB,
 	RCB,
-	LEFT
+	LEFT,
+  UNDO
 };
 
 const action_t PROGMEM fn_actions[] = {
@@ -50,7 +61,9 @@ const action_t PROGMEM fn_actions[] = {
   [4] = ACTION_LAYER_TOGGLE(3), //Game mode
   [5] = ACTION_MACRO(LCB),
   [6] = ACTION_MACRO(RCB),
-  [7] = ACTION_MACRO(LEFT)
+  [7] = ACTION_MACRO(LEFT),
+  [8] = ACTION_LAYER_TOGGLE(4), //Vim mode
+  [9] = ACTION_MACRO(UNDO),
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
@@ -76,6 +89,11 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 					MACRO( D(MINS), D(LSFT), D(DOT), END) :
 					MACRO( U(MINS), U(LSFT), U(DOT), END)
 				);
+    case UNDO:
+      return (record->event.pressed ?
+          MACRO( D(LCTRL), D(Z), END) :
+          MACRO( U(LCTRL), U(Z), END)
+        );
 
 	}
 	return MACRO_NONE;
